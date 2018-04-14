c8505e55afb8d1888c87ecbaa6ab3a32d8d986dc
chensijiang <121330950@qq.com>
2018-04-14 12:52:14 +0800

    Init commit.

---
+++ huobi-auto-app/src/main/java/io/yule/huobiauto/app/Application.java
@@ -0,0 +1,14 @@
+package io.yule.huobiauto.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by chensijiang on 2018/4/14 下午12:50.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+}
