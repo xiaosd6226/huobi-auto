15f70fabb28f24e7dbe375c80665d1f5bfedaf6d
chensijiang <121330950@qq.com>
2018-04-15 17:34:14 +0800

    web等配置。

---
+++ huobi-auto-app/src/main/java/io/yule/huobiauto/app/HuobiApiService.java
@@ -0,0 +1,17 @@
+package io.yule.huobiauto.app;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by chensijiang on 2018/4/15 下午4:55.
+ */
+@Component
+public class HuobiApiService {
+
+    @Value("${huobi.accessKey}")
+    private String accessKey;
+
+    @Value("${huobi.secretKey}")
+    private String secretKey;
+}
