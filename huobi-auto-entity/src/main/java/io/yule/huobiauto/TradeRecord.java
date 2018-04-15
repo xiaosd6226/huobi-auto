b6b71605e106190d1b67ac591356675177a85732
chensijiang <121330950@qq.com>
2018-04-15 17:45:14 +0800

    entity

---
+++ huobi-auto-entity/src/main/java/io/yule/huobiauto/TradeRecord.java
@@ -0,0 +1,12 @@
+package io.yule.huobiauto;
+
+import javax.persistence.Entity;
+import java.io.Serializable;
+
+/**
+ * Created by chensijiang on 2018/4/15 下午5:42.
+ */
+@Entity
+public class TradeRecord implements Serializable {
+    private static final long serialVersionUID = -7268972093503672321L;
+}
---
+++ huobi-auto-entity/src/main/java/io/yule/huobiauto/TradeTask.java
@@ -0,0 +1,12 @@
+package io.yule.huobiauto;
+
+import javax.persistence.Entity;
+import java.io.Serializable;
+
+/**
+ * Created by chensijiang on 2018/4/15 下午5:38.
+ */
+@Entity
+public class TradeTask implements Serializable {
+    private static final long serialVersionUID = -7812957286352275581L;
+}
